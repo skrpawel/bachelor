3c8555d57e2463cbd5416a49b785920d7dac33b3
Pawel <skrobskipawel@gmail.com>
2022-10-18 11:19:28 +0200

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import Typed from 'react-typed';
+// import Typed from 'react-typed';
 
 
 const Hero = () => {
@@ -9,7 +9,7 @@ const Hero = () => {
                 <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold">BE THE ATHLETE</h1>
                 <div className='flex justify-center items-center'>
                     <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4 '>Fast, Simple planner for </p>
-                    <Typed className="md:text-5xl sm:text-4xl text-xl font-bold pl-2 text-[#EB5E28]" strings={['RUNNERS', 'CYCLISTS', 'ATHLETES']} typeSpeed={120} backSpeed={140} loop />
+                    {/* <Typed className="md:text-5xl sm:text-4xl text-xl font-bold pl-2 text-[#EB5E28]" strings={['RUNNERS', 'CYCLISTS', 'ATHLETES']} typeSpeed={120} backSpeed={140} loop /> */}
                 </div>
                 <button className='bg-[#eb5e28] w-[200px] rounded-md font-medium mx-auto my-6 py-3'>Sign up</button>
             </div>
